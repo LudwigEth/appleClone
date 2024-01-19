d73feb2fcb2d64da0fb56a34330f0aaa9d165863
LudwigEth <ludwig.eth@gmail.com>
2024-01-19 16:02:59 +0100

    Initialize project with basic Webpack setup
    includes the configuration of Webpack to handle:
    JavaScript (using Babel),
    CSS, and asset files such as images, fonts, and videos.
    The project is equipped with HtmlWebpackPlugin for HTML file generation
    and CleanWebpackPlugin for cleaning the build directory.

---
+++ webpack.config.js
@@ -0,0 +1,55 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env'],
+                    },
+                },
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(mp4|webm)$/i,
+                type: 'asset/resource',
+            },
+        ],
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+        }),
+        new CleanWebpackPlugin(),
+    ],
+
+    devServer: {
+        contentBase: './dist',
+        open: true,
+    },
+};
