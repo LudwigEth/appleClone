520770db84e0fe339583d25c2dcad4a7f08cfe5f
Ludwig.eth <ludwig.eth@gmail.com>
2024-01-28 21:27:29 +0100

    Add rule to generate an images folder for the bundled images in dist

--- webpack.config.js
+++ webpack.config.js
@@ -29,6 +29,9 @@ module.exports = {
             {
                 test: /\.(png|svg|jpg|jpeg|gif)$/i,
                 type: 'asset/resource',
+                generator: {
+                    filename: 'images/[name].[hash][ext]'
+                }
             },
             {
                 test: /\.(woff|woff2|eot|ttf|otf)$/i,
