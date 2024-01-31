e54f60a16892682d5620e34bc947b57e423a3d3d
Ludwig.eth <ludwig.eth@gmail.com>
2024-01-31 13:03:53 +0100

    Add EventListener to Navigation Section and Clickhandler
    - Created dom element reference sectionNavigationLinks
    - Add clickeventlistener to sectionNavigationLinks
    - Add sectionNavigationLinksClickHandler
    this function is used as callback for the navigation clickhandler
    if the clicked element is a navigation-dropdown element,
    it will toggle the dropdown-expanded class on its parent container
    and trigger the dropDownSvgAnimation function, which fires off either
    the expand or collapse svg animate tag, depending on the containers class
    - Add dropDownSvgAnimation function
    Takes in a dom reference for the dropdown and the linkgroup
    decides to play either the expand or collapse animate tag, depending
    on the linkgroups class for dropdown-expanded

--- src/index.js
+++ src/index.js
@@ -1,1 +1,33 @@
 import './styles.css';
+
+const sectionNavigationLinks = document.getElementById('navigation-links');
+
+sectionNavigationLinks.addEventListener(
+    'click',
+    sectionNavigationLinksClickHandler
+);
+
+function sectionNavigationLinksClickHandler(e) {
+    const dropdown = e.target.closest('.navigation-dropdown');
+    const linkGroup = e.target.closest('.navigation-link-group');
+
+    if (dropdown && sectionNavigationLinks.contains(dropdown)) {
+        linkGroup.classList.toggle('dropdown-expanded');
+        dropDownSvgAnimation(dropdown, linkGroup);
+    }
+}
+
+function dropDownSvgAnimation(dropdown, linkGroup) {
+    const svgAnimateExpand = dropdown.querySelector(
+        'animate[data-footer-animate="expand"]'
+    );
+    const svgAnimateCollapse = dropdown.querySelector(
+        'animate[data-footer-animate="collapse"]'
+    );
+
+    if (linkGroup.classList.contains('dropdown-expanded')) {
+        svgAnimateExpand.beginElement();
+    } else {
+        svgAnimateCollapse.beginElement();
+    }
+}
