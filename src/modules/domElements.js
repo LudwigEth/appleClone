56917b6698d72385c23f66ad2dcf7587a40237aa
Ludwig.eth <ludwig.eth@gmail.com>
2024-02-01 22:57:58 +0100

    Add dom references for button-prev-image and button-next-image

--- src/modules/domElements.js
+++ src/modules/domElements.js
@@ -1,3 +1,5 @@
 export const sectionNavigationLinks =
     document.getElementById('navigation-links');
 export const imageCarousel = document.getElementById('image-carousel');
+export const buttonPrevImage = document.getElementById('button-prev-image');
+export const buttonNextImage = document.getElementById('button-next-image');
