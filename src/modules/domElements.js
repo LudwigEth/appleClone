c0a3468f186d9ec1c4d70164422fb1b7c27d0c87
Ludwig.eth <ludwig.eth@gmail.com>
2024-02-01 22:59:07 +0100

    Add dom reference for dot-navigation ul element

--- src/modules/domElements.js
+++ src/modules/domElements.js
@@ -3,3 +3,4 @@ export const sectionNavigationLinks =
 export const imageCarousel = document.getElementById('image-carousel');
 export const buttonPrevImage = document.getElementById('button-prev-image');
 export const buttonNextImage = document.getElementById('button-next-image');
+export const dotNavigation = document.getElementById('dot-navigation');
