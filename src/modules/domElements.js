847c9f1f12473ed704d187773884685cb35d1d19
Ludwig.eth <ludwig.eth@gmail.com>
2024-02-02 03:28:28 +0100

    Add dom element reference for the apple-tv section element

--- src/modules/domElements.js
+++ src/modules/domElements.js
@@ -1,5 +1,6 @@
 export const sectionNavigationLinks =
     document.getElementById('navigation-links');
+export const appleTvSection = document.getElementById('apple-tv');
 export const imageCarousel = document.getElementById('image-carousel');
 export const buttonPrevImage = document.getElementById('button-prev-image');
 export const buttonNextImage = document.getElementById('button-next-image');
