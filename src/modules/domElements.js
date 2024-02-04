8ee7d9edcc9db41ad61eccadfe7ad2f8b07d7dca
Ludwig.eth <ludwig.eth@gmail.com>
2024-02-04 20:43:14 +0100

    Format with prettier

--- src/modules/domElements.js
+++ src/modules/domElements.js
@@ -1,7 +1,7 @@
 export const sectionNavigationLinks =
-    document.getElementById('navigation-links');
-export const appleTvSection = document.getElementById('apple-tv');
-export const imageCarousel = document.getElementById('image-carousel');
-export const buttonPrevImage = document.getElementById('button-prev-image');
-export const buttonNextImage = document.getElementById('button-next-image');
-export const dotNavigation = document.getElementById('dot-navigation');
+    document.getElementById('navigation-links')
+export const appleTvSection = document.getElementById('apple-tv')
+export const imageCarousel = document.getElementById('image-carousel')
+export const buttonPrevImage = document.getElementById('button-prev-image')
+export const buttonNextImage = document.getElementById('button-next-image')
+export const dotNavigation = document.getElementById('dot-navigation')
